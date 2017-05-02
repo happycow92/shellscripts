c12c320b6d31c314a1402acd8a8d146c0b524b06
happycow92 <virtuallypeculiar@gmail.com>
2017-05-02 16:55:24 +0530

    Update proxy-fix.sh

--- proxy-fix.sh
+++ proxy-fix.sh
@@ -10,17 +10,17 @@ echo $value
 read -p "Proceed with fix?: " choice
 
 case "$choice" in
-y|Y )
-sed -i -e 's/entry key="ignore_vc_cert" value="false"/entry key="ignore_vc_cert" value="true"/g'  /usr/local/avamar/var/mc/server_data/prefs/mcserver.xml
-echo -e "\nThe new value for ignore_vc_cert is"
-value=$(cat /usr/local/avamar/var/mc/server_data/prefs/mcserver.xml | grep ignore_vc_cert | cut -d = -f 3 | cut -d " " -f 1)
-echo $value
-echo -e "\nProceeding to restart MCS"
-mcserver.sh --restart --verbose
-echo -e "\nProceeding to re-register internal proxy"
-sudo /usr/local/avamarclient/etc/initproxy.sh --start
-echo -e "\nAll done!";;
-n|N )
-echo -e "\nExiting script";;
+  y|Y )
+    sed -i -e 's/entry key="ignore_vc_cert" value="false"/entry key="ignore_vc_cert" value="true"/g'  /usr/local/avamar/var/mc/server_data/prefs/mcserver.xml
+    echo -e "\nThe new value for ignore_vc_cert is"
+    value=$(cat /usr/local/avamar/var/mc/server_data/prefs/mcserver.xml | grep ignore_vc_cert | cut -d = -f 3 | cut -d " " -f 1)
+    echo $value
+    echo -e "\nProceeding to restart MCS"
+    mcserver.sh --restart --verbose
+    echo -e "\nProceeding to re-register internal proxy"
+    sudo /usr/local/avamarclient/etc/initproxy.sh --start
+    echo -e "\nAll done!";;
+  n|N )
+    echo -e "\nExiting script";;
 esac
 
