5ccea50e5d7636db7acece3299b03638f2a4ae8e
Suhas G <virtuallypeculiar@gmail.com>
2017-08-29 03:11:38 +0530

    Update vR-jobs.sh

--- vR-jobs.sh
+++ vR-jobs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 
-cd /opt/vmware/vpostgres/9.3/bin
+cd /opt/vmware/vpostgres/9.*/bin
 echo "Backing VRMS Database under /tmp"
 ./pg_dump -U vrmsdb -Fp -c > /tmp/DBBackup.bak 
 echo
